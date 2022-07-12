3dcd2995981238f6338ca525731a714162c27393
StellaBellaO <101139955+StellaBellaO@users.noreply.github.com>
2022-07-12 10:46:52 +0100

    tested a different solution in order to get data and forecast to show

--- script.js
+++ script.js
@@ -79,26 +79,30 @@ function getForecast (coordinates) {
 
 /// Temperature ///
 function displayTemperature(response) {
-    document.querySelector("#city").innerHTML = response.data.name;
-    document.querySelector("#temperature").innerHTML = Math.round(
-      response.data.main.temp
-    );
-    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-    document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
-  
-    document.querySelector("#weather").innerHTML = response.data.weather[0].main;
-    celsiusTemperature = response.data.main.temp;
-    
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let weatherElement = document.querySelector("#weather");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
     let iconElement = document.querySelector("#icon");
-
+  
+    celsiusTemperature = response.data.main.temp;
+  
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+    cityElement.innerHTML = response.data.name;
+    weatherElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    timeElement.innerHTML = (response.data.dt * 1000);
     iconElement.setAttribute(
-        "src",
-        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-      );
-      iconElement.setAttribute("alt", response.data.weather[0].description);
-    
-      getForecast(response.data.coord);
-    }
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+  
+    getForecast(response.data.coord);
+  }
   
 
   /// Finding location ////
